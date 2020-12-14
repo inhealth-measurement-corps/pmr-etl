2d6a1ec875b992a85520105f84588e539a44cd3e
Mung Yao Jia <mungyaojia@Mung-Yaos-MacBook-Pro.local>
2020-12-14 17:59:41 -0500

    update act2hist and idata_automator python files

--- converter/act2hist.py
+++ converter/act2hist.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-changed on Aug 16 2019
-@author: Ben
+changed on Oct 26 2020
+@author: Mung Yao 
 """
 
 import pymssql
 
 # the new DB INFO
-host = 'ESMPMMCADBDEV1.win.ad.jhu.edu'
-username = 'pmr'
-password = 'kQSSD0J9gY4h2hj5OIPp'
-db = 'pmr'
+host = 'INHEALTH.win.ad.jhu.edu'
+username = 'WIN\WSE-MeasurementCorps'
+password = 'KwGCyTn97nSkFGaFnwP' 
+db = 'master'
 # connecting to the microsoft sql database
 connection = pymssql.connect(host=host, user=username, password=password, database=db)
 
 try:
     with connection.cursor() as cursor:
         #selects the live data in order to shorten the search for the patient info      
-        sql = "SELECT patient_ID FROM live_details"
+        sql = "SELECT patient_ID FROM mmambulation.live_details"
         cursor.execute(sql)
         result = cursor.fetchall()
 
@@ -37,27 +37,28 @@ try:
             newids.extend(id)
         #remove the duplicate elements
         newids = list(set(newids))
-        
+
         #determines if there is a discharge date and if so then appends another array with new ids
         changeids = []
         for i in range(len(newids)):
             new = newids[i]
-            ql = ("SELECT discharge_date FROM patient_info WHERE patient_ID= %d" %new)
+            ql = ("SELECT discharge_date FROM mmambulation.patient_info WHERE patient_ID= %d" %new)
             cursor.execute(ql)
             result1 = cursor.fetchall()
-            if result1 != ((None,),):
+            if result1 != [(None,)]:
                 changeids.append(new)
                 print(new)
 
-        # Create a new record based on the ids that have a discharge date
+         # Create a new record based on the ids that have a discharge date
         for i in range(len(changeids)):
             new = changeids[i]
-            cursor.execute("INSERT INTO historical_details (patient_ID, ambulation, date, time_of_day, distance, duration, speed) SELECT patient_ID, ambulation, amb_date, time_of_day, distance, duration, speed FROM live_details WHERE patient_ID = %d;" %new)
-            cursor.execute("DELETE FROM live_details WHERE patient_ID = %d;" %new)
+            cursor.execute("INSERT INTO mmambulation.historical_details (patient_ID, ambulation, date, time_of_day, distance, duration, speed) SELECT patient_ID, ambulation, date, time_of_day, distance, duration, speed FROM mmambulation.dante_live WHERE patient_ID = %d;" %new)
+            cursor.execute("DELETE FROM mmambulation.live_details WHERE patient_ID = %d;" %new)
 
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
+
+    # connection is not autocommit by default. So you must commit to save your changes. 
     connection.commit()
     
 finally:
     connection.close()
+
--- iData_automation/idata_automator.py
+++ iData_automation/idata_automator.py
@@ -4,10 +4,22 @@ import csv
 import os
 import mmap
 
-host = "ESMPMMCADBDEV1.win.ad.jhu.edu"
-username = "pmr"
-password = "kQSSD0J9gY4h2hj5OIPp"
-db = "pmr"
+host = 'INHEALTH.win.ad.jhu.edu'
+username = 'WIN\WSE-MeasurementCorps'
+password = 'KwGCyTn97nSkFGaFnwP' 
+db = 'master'
+
+file1 = open("test_record.txt", "a+")
+file1.write("Test Record \n") 
+file1.close()
+
+file2 = open("removed_patients_record.txt", "a+")
+file2.write("Removed Patients Record \n") 
+file2.close()
+
+file3 = open("IData.csv", "w")
+writer = csv.writer(file3)
+file3.close()
 # this automator's function is to transfer data from database(patient_info table) to IData.csv
 # test_record -> the patients who is still in the hospital
 # removed_patients_record -> the patients who have been discharged
@@ -20,7 +32,7 @@ cur = my_cnxn.cursor()
 
 while True: #continuous loop
     # get the patient_info table
-    cur.execute("SELECT * FROM patient_info")
+    cur.execute("SELECT * FROM mmambulation.live_details")
     # parameter '+a' means open a file for reading and writing
     # mmap's goal: Use a memory mapped file instead of reading the content directly.
     # fileno() is the file descriptor, parameter '0' means mapping the entire file, ACCESS_READ means read only
@@ -30,49 +42,58 @@ while True: #continuous loop
     file2 = open("removed_patients_record.txt","a+")
     s2 = mmap.mmap(file2.fileno(), 0, access = mmap.ACCESS_READ)
 
+    live_p = [] 
     for row in cur.fetchall():
-        #identifier combo of patient ID, room_number and admission date
-        identifier = str(row[0]) + ',' + str(row[1]) + ',' +str(row[2])
+        live_p.append(row[0])
 
-        date_and_time = str(row[3])
+    for i in range(len(live_p)):
+        new = live_p[i]
+        ql = ("SELECT * FROM mmambulation.patient_info WHERE patient_ID= %d" %new)
+        cur.execute(ql)
+        result1 = cur.fetchall()
+        for row in result1: 
+            identifier = str(row[0]) + ',' + str(row[1]) + ',' +str(row[2])
+            date_and_time = str(row[3])
+            discharge_date = str(row[6])
+            found_value_1 = s1.find(str.encode(identifier)) #is greater than -1 if the identifier exists in the file
+            print("The found value for patient " + str(row[0]) + " is " + str(found_value_1))
 
-        discharge_date = str(row[4])
+            if found_value_1 == -1 and discharge_date == 'None': #This is a new entry in the patient info table and they haven't been discharged yet
+                print("Patient " + str(row[0]) + " is a new entry.")
+                #we want to take the data and append it to a idata.csv file
 
-        found_value_1 = s1.find(str.encode(identifier)) #is greater than -1 if the identifier exists in the file
-        print("The found value for patient " + str(row[0]) + " is " + str(found_value_1))
+                line_ender = "N/A</br>" # thing we need to end the line with
 
-        if found_value_1 == -1 and discharge_date == None: #This is a new entry in the patient info table and they haven't been discharged yet
-            print("Patient " + str(row[0]) + " is a new entry.")
-            #we want to take the data and append it to a idata.csv file
+                insert_row = [row[0], row[1], date_and_time, line_ender]
+                #with open('IData.csv', 'a',encoding='utf-8') as csvFile:
+                with open('IData.csv', 'a') as csvFile:
+                    writer = csv.writer(csvFile)
+                    writer.writerow(insert_row)
 
-            line_ender = "N/A</br>" # thing we need to end the line with
+                # add the id and date thing to the txt file
+                file1.write(identifier + '\n')
 
-            insert_row = [row[0], row[1], date_and_time, line_ender]
-            with open('IData.csv', 'a',encoding='utf-8') as csvFile:
-                writer = csv.writer(csvFile)
-                writer.writerow(insert_row)
+            found_value_2 = s2.find(str.encode(identifier)) #is greater than -1 if the identifier exists in the file
 
-            # add the id and date thing to the txt file
-            file1.write(identifier + '\n') 
+            if discharge_date != 'None' and found_value_2 == -1: #patient has been discharged, they haven't been removed from IData.csv yet and should be
+                print ("Patient " + str(row[0]) + " was discharged.")
+                discharged_identifier = str(row[0]) 
+                lines = list()
+                with open('IData.csv', 'r') as csvFile:
+                    reader = csv.reader(csvFile)
+                    for row in reader:
+                        if discharged_identifier != row[0]:
+                            lines.append(row)
 
-        found_value_2 = s2.find(str.encode(identifier)) #is greater than -1 if the identifier exists in the file
+                with open('IData.csv', 'w') as writeFile:
+                    writer = csv.writer(writeFile)
+                    writer.writerows(lines)
 
-        if discharge_date != None and found_value_2 == -1: #patient has been discharged, they haven't been removed from IData.csv yet and should be
-            print("patient " + str(row[0]) + " has been discharged")
-            with open("IData.csv", "r") as f:
-                lines = f.readlines()
-            with open("IData.csv", "w") as f:
-                for line in lines:
-                    csf_row_string = str(row[0]) + "," +  str(row[1]) + ","  + date_and_time
-                    if line.strip("\n") != csf_row_string:
-                        f.write(line)
-                    else:
-                        print("didn't write a line.")
-            file2.write(identifier + '\n') # add this discharged patient into the removed_patients_record
-        # print(type(str(ambulation.start_datetime)))
+                file2.write(identifier + '\n') # add this discharged patient into the removed_patients_record
 
     file1.close()
     file2.close()
     print("ending loop")
-    time.sleep(60) #Make it loop every mintue
+    time.sleep(10) #Make it loop every mintue
     print(" ")
+
